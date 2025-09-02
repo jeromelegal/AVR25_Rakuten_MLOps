37d53dc9b81c3ac4244d9fbce38dc2ed098ea6e4
JF PONS <jeff.pons2@gmail.com>
2025-09-02 18:35:22 +0200

    feat: add Minio service

---
+++ src/minio/minio/init-minio.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Lauch Minio server
+echo "Starting Minio server..."
+minio server /data --address ":$PORT" --console-address ":$GUI_PORT" &
+HEALTH_URL=http://127.0.0.1:$PORT/minio/health/live
+HTTP_CODE=$(curl -k -o /dev/null -s -w "%{http_code}\n" $HEALTH_URL)
+until [ $HTTP_CODE -eq 200 ]; do
+    HTTP_CODE=$(curl -k -o /dev/null -s -w "%{http_code}\n" $HEALTH_URL)
+    echo "Waiting for Minio service to be healthy."
+    sleep 1
+done
+echo "Minio server started"
+
+# Define alias for the following commands
+ALIAS="myminio"
+HOSTNAME="http://127.0.0.1:$PORT"
+
+bash +o history
+mc alias set $ALIAS $HOSTNAME $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+bash -o history
+echo "Minio alias configured"
+
+# Exit if there is an error code when checking connection to the server
+echo "Checking Minio connection..."
+mc admin info "$ALIAS"
+STATUS=$?
+
+if [ $STATUS -ne 0 ]; then
+    echo "Failed to retrieve MinIO admin info for alias '$ALIAS'"
+    exit $STATUS
+fi
+echo "Minio connection OK"
+
+# Generate bucket and exit if not possible
+echo "Checking Minio bucket..."
+mc mb --ignore-existing --region "eu-central-1" /data/$MINIO_BUCKET_NAME
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo "Failed to create Minio bucket '$MINIO_BUCKET_NAME'"
+    exit $STATUS
+fi
+echo "Minio bucket OK"
