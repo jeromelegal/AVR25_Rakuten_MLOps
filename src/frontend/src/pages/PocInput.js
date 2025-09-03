12afa38954374d700efa12212f6876931ee25ca8
pyg <pierre-yves.g@obat.fr>
2025-09-03 08:48:22 +0200

    add components input text and image in page poc

--- src/frontend/src/pages/PocInput.js
+++ src/frontend/src/pages/PocInput.js
@@ -1,8 +1,27 @@
 // frontend/src/pages/PocInput.js
-import React from 'react';
+import React, {useState} from 'react';
+import ProductDescriptionInput from '../components/ProductDescriptionInput';
+import ProductPictureInput from '../components/ProductPictureInput';
 
 const PocInput = () => {
-  return <div>PocInput Page</div>;
+  const [description, setDescription] = useState('');
+  const [picture, setPicture] = useState(null);
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handlePictureChange = (e) => {
+    setPicture(e.target.files[0]);
+  };
+
+  return (
+    <div>
+      <h1>PocInput Page</h1>
+      <ProductDescriptionInput value={description} onChange={handleDescriptionChange} />
+      <ProductPictureInput onChange={handlePictureChange} />
+    </div>
+  );
 };
 
 export default PocInput;
