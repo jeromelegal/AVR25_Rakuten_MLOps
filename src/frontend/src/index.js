3f21c2e87c23d374a84561645299581bffbceafd
pyg <pierre-yves.g@obat.fr>
2025-09-08 16:39:30 +0200

    fix useless import

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,5 +1,4 @@
 // frontend/src/index.js
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/frontend/src/pages/NewProduct.js
+++ src/frontend/src/pages/NewProduct.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import ProductDescriptionInput from '../components/ProductDescriptionInput';
 import ProductPictureInput from '../components/ProductPictureInput';
 import ProductTitleInput from '../components/ProductTitleInput';
