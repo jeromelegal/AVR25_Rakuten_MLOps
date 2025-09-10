6dc75b8c9762981a2c8bad50df18b68b5f899272
JF PONS <jeff.pons2@gmail.com>
2025-09-10 07:26:04 +0200

    feat(api-text-porcessing): load the artifacts from MLFLow

--- src/models/api-text-processing/api/config/model_loader.py
+++ src/models/api-text-processing/api/config/model_loader.py
@@ -22,14 +22,28 @@ def get_translator_model(settings: Annotated[Settings, Depends(get_settings)]):
     model_version = settings.MLFLOW_TEXT_TRANSLATOR_MODEL_VERSION
 
     mlflow.set_tracking_uri(settings.MLFLOW_ADDR)
+
+    mlflow.artifacts.download_artifacts(
+        run_id=settings.MLFLOW_TEXT_TRANSLATOR_ARTIFACT_RUN_ID,
+        artifact_path=settings.MLFLOW_TEXT_TRANSLATOR_CACHE_ARTIFACT_PATH,
+        dst_path=settings.MLFLOW_LOCAL_ARTIFACT_DIRECTORY_PATH,
+    )
+
     return mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
 
 
 @lru_cache
 def get_language_detector_model(settings: Annotated[Settings, Depends(get_settings)]):
 
-    model_name = settings.MLFL
-    model_version = settings.MLFLOW_TEXT_TRANSLATOR_MODEL_VERSION
+    model_name = settings.MLFLOW_TEXT_LANGUAGE_DETECTOR_MODEL_NAME
+    model_version = settings.MLFLOW_TEXT_LANGUAGE_DETECTOR_MODEL_VERSION
 
     mlflow.set_tracking_uri(settings.MLFLOW_ADDR)
+
+    mlflow.artifacts.download_artifacts(
+        run_id=settings.MLFLOW_TEXT_LANGUAGE_DETECTOR_ARTIFACT_RUN_ID,
+        artifact_path=settings.MLFLOW_TEXT_LANGUAGE_DETECTOR_INDEX_ARTIFACT_PATH,
+        dst_path=settings.MLFLOW_LOCAL_ARTIFACT_DIRECTORY_PATH,
+    )
+
     return mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
