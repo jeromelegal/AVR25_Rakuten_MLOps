daa1416c188ae9cb47081788d39635372d924a06
JF PONS <jeff.pons2@gmail.com>
2025-09-10 11:11:40 +0200

    test(api-image-processing): change mlflow mock to use tensorflow

--- src/models/api-image-processing/test/api/config/test_model_loader.py
+++ src/models/api-image-processing/test/api/config/test_model_loader.py
@@ -7,7 +7,7 @@ class TestModelLoader:
     @patch("api.config.model_loader.mlflow", return_value=Mock())
     def test_get_image_classifier_model(self, mlflow_mock):
         mocked_model = Mock()
-        mlflow_mock.pyfunc.load_model.return_value = mocked_model
+        mlflow_mock.tensorflow.load_model.return_value = mocked_model
 
         model = get_image_classifier_model(settings=Mock())
 
