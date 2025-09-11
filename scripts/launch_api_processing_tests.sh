0f68f0150009db661647f7b125121f3468b6631d
JF PONS <jeff.pons2@gmail.com>
2025-09-11 14:22:39 +0200

    test(models): include text, image and combined processing api tests and lint to CI

--- scripts/launch_api_processing_tests.sh
+++ scripts/launch_api_processing_tests.sh
@@ -2,13 +2,13 @@
 
 SUFFIX="$1"
 RESULT_FOLDER=test_results
-CONTAINER_NAME="api-text-processing$SUFFIX"
+CONTAINER_NAME="api-processing$SUFFIX"
 # Create folder to store test results
 echo "Create test result folder"
 mkdir -p $RESULT_FOLDER
 
-# Execute api-text-processing tests
-echo "Executing api-text-processing tests in container $CONTAINER_NAME"
+# Execute api-processing tests
+echo "Executing api-processing tests in container $CONTAINER_NAME"
 docker exec $CONTAINER_NAME pip3 install --break-system-packages pytest-cov && \
 docker exec $CONTAINER_NAME python3 -m pytest \
         --junit-xml=test_result.xml \
@@ -16,5 +16,5 @@ docker exec $CONTAINER_NAME python3 -m pytest \
         --cov-report xml:test_coverage.xml \
         --cov-report term \
         --rootdir /app && \
-docker cp $CONTAINER_NAME:/app/test_result.xml $RESULT_FOLDER/api-text-processing_test_result.xml  && \
-docker cp $CONTAINER_NAME:/app/test_coverage.xml $RESULT_FOLDER/api-text-processing_test_coverage.xml
+docker cp $CONTAINER_NAME:/app/test_result.xml $RESULT_FOLDER/api-processing_test_result.xml  && \
+docker cp $CONTAINER_NAME:/app/test_coverage.xml $RESULT_FOLDER/api-processing_test_coverage.xml
