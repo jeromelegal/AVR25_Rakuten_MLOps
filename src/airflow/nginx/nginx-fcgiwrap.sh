521cb9a11e2085ea89afeef11b87eb5fd3b04546
pyg <pierre-yves.g@obat.fr>
2025-09-11 12:05:25 +0200

    set up

---
+++ src/airflow/nginx/nginx-fcgiwrap.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Chemin du socket fcgiwrap
+SOCKET_PATH="/var/run/fcgiwrap/fcgiwrap.socket"
+
+# Créer le répertoire pour le socket si nécessaire
+mkdir -p $(dirname "$SOCKET_PATH")
+
+# Démarrer fcgiwrap en arrière-plan
+/usr/sbin/fcgiwrap -s unix:$SOCKET_PATH &
+FCGIWRAP_PID=$!
+
+# Attendre que le socket soit créé
+until [ -e "$SOCKET_PATH" ]; do
+  echo "Attente de la création du socket $SOCKET_PATH..."
+  sleep 1
+done
+
+# Définir les permissions pour le socket et le script de vérification de santé
+chown nobody:nogroup $SOCKET_PATH /usr/local/bin/health-check.sh
+chmod 660 $SOCKET_PATH /usr/local/bin/health-check.sh
+chmod g+w $SOCKET_PATH /usr/local/bin/health-check.sh
+chmod +x /usr/local/bin/health-check.sh
+
+# Générer la configuration Nginx
+nginx-conf.sh
+
+# Tester la configuration Nginx
+nginx -t
+
+# Démarrer Nginx
+nginx
+NGINX_PID=$!
+
+# Vérifier la santé du service
+HTTP_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" https://$SERVICE_NAME/health)
+
+# Afficher le code de réponse
+echo "HTTP Code: $HTTP_CODE"
+
+# Vérifier si le service est sain
+until [ $HTTP_CODE -eq 200 ]; do
+  HTTP_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" https://$SERVICE_NAME/health)
+  # Afficher le code de réponse
+  echo "HTTP Code: $HTTP_CODE"
+  echo "Service is unhealthy."
+  sleep 1
+done
+
+
+echo "Service is healthy."
+
---
+++ src/airflow/vault/vault.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+
+# Appeler le script Vault pour récupérer les certificats et la clé privée
+HTTP_CODE=$(curl -k -o /dev/null -s -w "%{http_code}\n" https://$VAULT_SERVICE_NAME/health)
+# Vous pouvez ajouter une logique conditionnelle ici
+until [ $HTTP_CODE -eq 200 ]; do
+    HTTP_CODE=$(curl -k -o /dev/null -s -w "%{http_code}\n" https://$VAULT_SERVICE_NAME/health)
+    echo "Waiting for Vault service to be healthy."
+    sleep 1
+done
+
+# Se connecter à Vault et récupérer un token
+export VAULT_SKIP_VERIFY="1"
+
+mkdir -p $(dirname $MINIO_PEM_PATH)  $(dirname $MINIO_CA_PATH) $(dirname $MINIO_MINIO_PEM_PATH) $(dirname $MINIO_MINIO_CA_PATH) 
+
+until vault login -method=userpass username=$VAULT_USERNAME password=$VAULT_PASSWORD > /dev/null; do
+    echo "Échec de l'authentification. Nouvelle tentative dans 1 secondes..."
+    sleep 1
+done
+
+# Récupérer les CA à ajouter aux magasins
+vault kv get -field=certificate secret/vault/ca > vault_ca.crt
+vault kv get -field=certificate secret/consul/ca > consul_ca.crt
+vault kv get -field=certificate secret/minio/ca > minio_ca.crt
+
+cp minio_ca.crt $MINIO_CA_PATH
+
+# Ajouter les CA aux magasins de certificats
+cp vault_ca.crt /usr/local/share/ca-certificates/
+cp consul_ca.crt /usr/local/share/ca-certificates/
+cp minio_ca.crt /usr/local/share/ca-certificates/
+
+update-ca-certificates
+
+export VAULT_SKIP_VERIFY="0"
+
+ips=$(ip -o -4 addr list | awk '{print $4}' | cut -d/ -f1)
+
+# Variable pour stocker les adresses IP séparées par des virgules
+ip_list=""
+# Boucle pour traiter chaque adresse IP
+for ip in $ips; do
+  if [ -z "$ip_list" ]; then
+    ip_list="$ip"
+  else
+    ip_list="$ip_list,$ip"
+  fi
+done
+
+vault write -format=json pki_minio/issue/minio common_name="minio" ip_sans="$ip_list" alt_names="$SERVICE_NAME" ttl="72h" > minio_cert.json
+
+cat <<EOF > $MINIO_PEM_PATH
+$(jq -r '.data.private_key' minio_cert.json)
+$(jq -r '.data.certificate' minio_cert.json)
+$(jq -r '.data.issuing_ca' minio_cert.json)
+EOF
+
+# Extraire le certificat et la clé privée
+
+echo "MINIO_CERT_PATH = $MINIO_CERT_PATH"
+cat <<EOF > $MINIO_CERT_PATH
+$(jq -r '.data.certificate' minio_cert.json)
+EOF
+echo "MINIO_KEY_PATH = $MINIO_KEY_PATH"
+cat <<EOF > $MINIO_KEY_PATH
+$(jq -r '.data.private_key' minio_cert.json)
+EOF
+
+# Définir les permissions pour les fichiers de certificat et de clé
+chown minio:minio $MINIO_PEM_PATH
+chmod 400 $MINIO_PEM_PATH
+
+# Nettoyage des fichiers temporaires
+rm -f mongo_service.json
+
+# Vérifier si le certificat et la clé Backend existent déjà
+if vault kv get -field=cert secret/minio/minio/certs > /dev/null 2>&1 && vault kv get -field=key secret/minio/minio/certs > /dev/null 2>&1; then
+  echo "Le certificat et la clé Backend existent déjà"
+  MINIO_MINIO_CA=$(vault kv get -field=ca secret/minio/minio/certs)
+  MINIO_MINIO_CERT=$(vault kv get -field=cert secret/minio/minio/certs)
+  MINIO_MINIO_KEY=$(vault kv get -field=key secret/minio/minio/certs)  
+
+else
+  # Générer le certificat et la clé pour Backend
+  echo "Générer le certificat et la clé pour Backend"
+  vault write -format=json pki_minio/issue/minio common_name="minio"   ttl="72h" > minio_minio_cert.json
+
+  # Extraire le certificat et la clé privée
+  MINIO_MINIO_CA=$(jq -r '.data.ca_chain' minio_minio_cert.json)
+  MINIO_MINIO_CERT=$(jq -r '.data.certificate' minio_minio_cert.json)
+  MINIO_MINIO_KEY=$(jq -r '.data.private_key' minio_minio_cert.json)
+  
+  # Enregistrer le certificat et la clé privée dans Vault
+  vault kv put secret/minio/minio/certs cert="$MINIO_MINIO_CERT" key="$MINIO_MINIO_KEY" ca="$MINIO_MINIO_CA"
+  # Nettoyage des fichiers temporaires
+  rm -f minio_minio_cert.json
+fi
+
+cat <<EOF > $MINIO_MINIO_PEM_PATH
+$(printf "%s" "$MINIO_MINIO_KEY")
+$(printf "%s" "$MINIO_MINIO_CERT")
+EOF
+
+printf "%s" $MINIO_MINIO_CA > $MINIO_MINIO_CA_PATH
+
+key_value=$(openssl rand -base64 741 | tr -d '\n' )
+
+vault kv put secret/minio/keyfile value=$key_value
+
+retrieve=$(vault kv get -field=value secret/minio/keyfile)
+
+mkdir -p /etc/ssl/minio/
+echo $retrieve > /etc/ssl/minio/minio-keyfile
+chmod 600 /etc/ssl/minio/minio-keyfile
+
+# Vérifier si le certificat et la clé Vault existent déjà
+if vault kv get -field=cert secret/minio/api-minio/certs > /dev/null 2>&1 && vault kv get -field=key secret/minio/api-minio/certs > /dev/null 2>&1; then
+  echo "Le certificat mTLS minio pour le service api-minio existe déjà"
+else
+  # Générer le certificat et la clé pour Vault
+  echo "Générer le certificat et la clé pour Vault"
+  vault write -format=json pki_minio/issue/minio common_name="minio"   ttl="72h" > minio_api-minio_cert.json
+  # TODO: Define certificate duration as an env variable
+
+  # Extraire le certificat et la clé privée
+  MINIO_API_MINIO_CA=$(jq -r '.data.ca_chain[0]' minio_api-minio_cert.json)
+  MINIO_API_MINIO_CERT=$(jq -r '.data.certificate' minio_api-minio_cert.json)
+  MINIO_API_MINIO_KEY=$(jq -r '.data.private_key' minio_api-minio_cert.json)
+
+  # Enregistrer le certificat et la clé privée dans Vault
+  vault kv put secret/minio/api-minio/certs cert="$MINIO_API_MINIO_CERT" key="$MINIO_API_MINIO_KEY" ca="$MINIO_API_MINIO_CA"
+
+  # Nettoyage des fichiers temporaires
+  rm -f vault_service_cert.json
+fi
