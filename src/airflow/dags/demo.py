78b52fbaac081a0f3487719fbfc754d5da357c0a
pyg <pierre-yves.g@obat.fr>
2025-09-19 10:31:51 +0200

    fix demo

--- src/airflow/dags/demo.py
+++ src/airflow/dags/demo.py
@@ -1,14 +1,17 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+import logging
 
 def say_hello():
-    print("Hello world from Airflow!")
+    
+    logger = logging.getLogger("airflow.task")
+    logger.info("✅ Hello world from Airflow!")
 
 with DAG(
     dag_id="hello_world_dag",
     start_date=datetime(2023, 1, 1),
-
+    schedule=None,
     catchup=False,
     tags=["test", "hello"],
 ) as dag:
