937ebcdf7407d477a2d1f1a1209ddc41b3722ac0
JF PONS <jeff.pons2@gmail.com>
2025-09-21 15:19:30 +0200

    feat(alertmanager): add alertmanager service

---
+++ src/alertmanager/nginx/nginx-conf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat <<EOF > $NGINX_CONFIG_FILE
+events {
+    worker_connections 1024; # Nombre de connexions simultanées qu'un worker peut gérer
+}
+
+http {
+    server {
+        listen 443 ssl;
+        server_name $SERVICE_NAME;
+
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ciphers HIGH:!aNULL:!MD5;
+        ssl_certificate $ALERTMANAGER_CERT_PATH;
+        ssl_certificate_key $ALERTMANAGER_KEY_PATH;
+
+        location /health {
+            include fastcgi_params;
+            fastcgi_pass unix:/var/run/fcgiwrap/fcgiwrap.socket;
+            fastcgi_param SCRIPT_FILENAME $HEALTH_CHECK_FILE;
+            fastcgi_param DOCUMENT_ROOT /usr/local/bin;
+            fastcgi_param SCRIPT_NAME health-check.sh;
+            fastcgi_param QUERY_STRING "";
+        }
+
+        # Autres configurations de votre serveur
+    }
+}
+EOF
