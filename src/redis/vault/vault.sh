fea2ab2e234a5d805899d41eaddf87144b2f2523
JF PONS <jeff.pons2@gmail.com>
2025-09-23 13:53:25 +0200

    feat(redis): preparing redis vault configuration for airflow services

--- src/redis/vault/vault.sh
+++ src/redis/vault/vault.sh
@@ -77,96 +77,60 @@ chmod 400 $REDIS_PEM_PATH
 # Nettoyage des fichiers temporaires
 rm -f redis_cert.json
 
-# Vérifier si le certificat et la clé Backend existent déjà
-if vault kv get -field=cert secret/redis/redis/certs > /dev/null 2>&1 && vault kv get -field=key secret/redis/redis/certs > /dev/null 2>&1; then
-  echo "Le certificat et la clé Backend existent déjà"
-  REDIS_REDIS_CA=$(vault kv get -field=ca secret/redis/redis/certs)
-  REDIS_REDIS_CERT=$(vault kv get -field=cert secret/redis/redis/certs)
-  REDIS_REDIS_KEY=$(vault kv get -field=key secret/redis/redis/certs)  
+#Liste des services pour lesquels générer les certificats
+services=("${SERVICE_NAME}")
 
-else
-  # Générer le certificat et la clé pour Backend
-  echo "Générer le certificat et la clé pour Backend"
-  vault write -format=json pki_redis/issue/redis common_name="redis"   ttl="72h" > redis_redis_cert.json
+# Boucle sur chaque service
+for service_name in "${services[@]}"; do
+  echo "Traitement du service : $service_name"
 
-  # Extraire le certificat et la clé privée
-  REDIS_REDIS_CA=$(jq -r '.data.ca_chain' redis_redis_cert.json)
-  REDIS_REDIS_CERT=$(jq -r '.data.certificate' redis_redis_cert.json)
-  REDIS_REDIS_KEY=$(jq -r '.data.private_key' redis_redis_cert.json)
+  # Vérifier si le certificat et la clé Vault existent déjà
+  if vault kv get -field=cert "secret/${SERVICE_NAME}/${service_name}/certs" > /dev/null 2>&1 && \
+     vault kv get -field=key "secret/${SERVICE_NAME}/${service_name}/certs" > /dev/null 2>&1; then
+    echo "Le certificat mTLS ${SERVICE_NAME} pour le ${service_name} existe déjà"
+    CA=$(vault kv get -field=ca "secret/${SERVICE_NAME}/${service_name}/certs")
+    CERT=$(vault kv get -field=cert "secret/${SERVICE_NAME}/${service_name}/certs")
+    KEY=$(vault kv get -field=key "secret/${SERVICE_NAME}/${service_name}/certs")
+  else
+    # Générer le certificat et la clé pour Vault
+    echo "Générer le certificat et la clé pour Vault pour ${service_name}"
+    vault write -format=json pki_${SERVICE_NAME}/issue/${SERVICE_NAME} common_name="${SERVICE_NAME}" ttl="72h" > "${SERVICE_NAME}_${service_name}_cert.json"
+
+    # Extraire le certificat et la clé privée
+    CA=$(jq -r '.data.ca_chain[0]' "${SERVICE_NAME}_${service_name}_cert.json")
+    CERT=$(jq -r '.data.certificate' "${SERVICE_NAME}_${service_name}_cert.json")
+    KEY=$(jq -r '.data.private_key' "${SERVICE_NAME}_${service_name}_cert.json")
+
+    # Enregistrer le certificat et la clé privée dans Vault
+    vault kv put "secret/${SERVICE_NAME}/${service_name}/certs" cert="$CERT" key="$KEY" ca="$CA"
+
+    # Nettoyage des fichiers temporaires
+    rm -f "${SERVICE_NAME}_${service_name}_cert.json"
+  fi
   
-  # Enregistrer le certificat et la clé privée dans Vault
-  vault kv put secret/redis/redis/certs cert="$REDIS_REDIS_CERT" key="$REDIS_REDIS_KEY" ca="$REDIS_REDIS_CA"
-  # Nettoyage des fichiers temporaires
-  rm -f redis_redis_cert.json
-fi
+  
+  # Écrire les fichiers de certificats et clés
+  KEY_PATH="/etc/ssl/${SERVICE_NAME}/${SERVICE_NAME}_${service_name}.key"
+  CERT_PATH="/etc/ssl/${SERVICE_NAME}/${SERVICE_NAME}_${service_name}.crt"
+  PEM_PATH="/etc/ssl/${SERVICE_NAME}/${SERVICE_NAME}_${service_name}.pem"
+  CA_PATH="/etc/ssl/${SERVICE_NAME}/${SERVICE_NAME}_${service_name}_ca.crt"
 
-cat <<EOF > $REDIS_REDIS_PEM_PATH
-$(printf "%s" "$REDIS_REDIS_KEY")
-$(printf "%s" "$REDIS_REDIS_CERT")
+  cat <<EOF > "${KEY_PATH}"
+$(printf "%s" "$KEY")
 EOF
 
-printf "%s" $REDIS_REDIS_CA > $REDIS_REDIS_CA_PATH
+  cat <<EOF > "${CERT_PATH}"
+$(printf "%s" "$CERT")
+EOF
 
-key_value=$(openssl rand -base64 741 | tr -d '\n' )
+  cat <<EOF > "${PEM_PATH}"
+$(printf "%s" "$KEY")
+$(printf "%s" "$CERT")
+EOF
 
-vault kv put secret/redis/keyfile value=$key_value
+  cat <<EOF > "${CA_PATH}"
+$(printf "%s" "$CA")
+EOF
 
-retrieve=$(vault kv get -field=value secret/redis/keyfile)
-
-mkdir -p /etc/ssl/redis/
-echo $retrieve > /etc/ssl/redis/redis-keyfile
-chmod 600 /etc/ssl/redis/redis-keyfile
-
-# # Vérifier si le certificat et la clé Vault existent déjà
-# if vault kv get -field=cert secret/redis/mlflow/certs > /dev/null 2>&1 && vault kv get -field=key secret/redis/mlflow/certs > /dev/null 2>&1; then
-#   echo "Le certificat mTLS redis pour le service mlflow existe déjà"
-#   REDIS_MLFLOW_CA=$(vault kv get -field=ca secret/redis/mlflow/certs)
-#   REDIS_MLFLOW_CERT=$(vault kv get -field=cert secret/redis/mlflow/certs)
-#   REDIS_MLFLOW_KEY=$(vault kv get -field=key secret/redis/mlflow/certs)  
-# else
-#   # Générer le certificat et la clé pour Vault
-#   echo "Générer le certificat et la clé pour Vault"
-#   vault write -format=json pki_redis/issue/redis common_name="redis"   ttl="72h" > redis_mlflow_cert.json
-#   # TODO: Define certificate duration as an env variable
-
-#   # Extraire le certificat et la clé privée
-#   REDIS_MLFLOW_CA=$(jq -r '.data.ca_chain[0]' redis_mlflow_cert.json)
-#   REDIS_MLFLOW_CERT=$(jq -r '.data.certificate' redis_mlflow_cert.json)
-#   REDIS_MLFLOW_KEY=$(jq -r '.data.private_key' redis_mlflow_cert.json)
-
-#   # Enregistrer le certificat et la clé privée dans Vault
-#   vault kv put secret/redis/mlflow/certs cert="$REDIS_MLFLOW_CERT" key="$REDIS_MLFLOW_KEY" ca="$REDIS_MLFLOW_CA"
-#   # Nettoyage des fichiers temporaires
-#   rm -f vault_service_cert.json
-# fi
-
-# # Vérifier si le certificat et la clé Vault existent déjà
-# if vault kv get -field=cert secret/redis/airflow/certs > /dev/null 2>&1 && vault kv get -field=key secret/redis/airflow/certs > /dev/null 2>&1; then
-#   echo "Le certificat mTLS redis pour le service airflow existe déjà"
-#   REDIS_AIRFLOW_CA=$(vault kv get -field=ca secret/redis/airflow/certs)
-#   REDIS_AIRFLOW_CERT=$(vault kv get -field=cert secret/redis/airflow/certs)
-#   REDIS_AIRFLOW_KEY=$(vault kv get -field=key secret/redis/airflow/certs)
-# else
-#   # Générer le certificat et la clé pour Vault
-#   echo "Générer le certificat et la clé pour Vault"
-#   vault write -format=json pki_redis/issue/redis common_name="redis"   ttl="72h" > redis_airflow_cert.json
-#   # TODO: Define certificate duration as an env variable
-
-#   # Extraire le certificat et la clé privée
-#   REDIS_AIRFLOW_CA=$(jq -r '.data.ca_chain[0]' redis_airflow_cert.json)
-#   REDIS_AIRFLOW_CERT=$(jq -r '.data.certificate' redis_airflow_cert.json)
-#   REDIS_AIRFLOW_KEY=$(jq -r '.data.private_key' redis_airflow_cert.json)
-
-#   # Enregistrer le certificat et la clé privée dans Vault
-#   vault kv put secret/redis/airflow/certs cert="$REDIS_AIRFLOW_CERT" key="$REDIS_AIRFLOW_KEY" ca="$REDIS_AIRFLOW_CA"
-#   # Nettoyage des fichiers temporaires
-#   rm -f vault_service_cert.json
-# fi
-
-# cat <<EOF > $REDIS_MLFLOW_PEM_PATH
-# $(printf "%s" "$REDIS_MLFLOW_KEY")
-# $(printf "%s" "$REDIS_MLFLOW_CERT")
-# EOF
-
-# printf "%s" $REDIS_MLFLOW_CA > $REDIS_MLFLOW_CA_PATH
-
+  echo "Traitement terminé pour le service : $service_name"
+done
